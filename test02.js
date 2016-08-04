a930ddbd5c2ddd5976d118f6b5f68b56e383669f
Mario <mario@users.noreply.github.com>
2016-08-04 19:15:05 +0200

    edited test02.js so that i can so a new commit so that i can delete the commit that did not work because of a file that is too big for pushing it to GitHub

--- test02.js
+++ test02.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 var test02;
+=======
+var test02='test02';
+var schwachsinn='was für ein scheiß';
+>>>>>>> 3a0f3df... edited test02.js so that i can so a new commit so that i can delete the commit that did not work because of a file that is too big for pushing it to GitHub
